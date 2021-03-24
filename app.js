81d9d7f35d55a9c00695ed910cbdbf6e578afa20
Titose Chembezi <titosechembezi@gmail.com>
2021-03-24 17:39:37 +0200

    Added code for web server to find static files in the public folder

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require("path");
 const router = express.Router();
 
+//serve static files(that support you apps. The next line of code tells your webserver where to find it) in express
+app.use(express.static(path.join(__dirname, "public")));
+
 router.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/index.html"));
   //__dirname : It will resolve to your project folder.
